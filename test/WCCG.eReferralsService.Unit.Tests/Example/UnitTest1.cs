4cd001f571faa0f3a2b1d894f0580a7aeefaeece
Patryk Urban <patryku@kainos.com>
2025-02-03 15:40:34 +0100

    Working base project

---
+++ test/WCCG.eReferralsService.Unit.Tests/Example/UnitTest1.cs
@@ -0,0 +1,12 @@
+using FluentAssertions;
+
+namespace WCCG.eReferralsService.Unit.Tests.Example;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        true.Should().BeTrue();
+    }
+}
