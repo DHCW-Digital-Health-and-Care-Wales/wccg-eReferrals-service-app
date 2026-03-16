4bd93f639ca8697c7215f07f61e7b69e1a8ee6e0
Dawid Richert <dawidr@kainos.com>
2026-03-16 23:50:43 +0100

    Updated WPAS api calls error handling

---
+++ src/NWRI.eReferralsService.API/Errors/NotSuccessfulWpasApiResponseError.cs
@@ -0,0 +1,11 @@
+using Hl7.Fhir.Model;
+using NWRI.eReferralsService.API.Constants;
+
+namespace NWRI.eReferralsService.API.Errors;
+
+public class NotSuccessfulWpasApiResponseError : BaseFhirHttpError
+{
+    public override string Code => FhirHttpErrorCodes.ReceiverServerError;
+    public override string DiagnosticsMessage => "WPAS API returned an unsuccessful response.";
+    public override OperationOutcome.IssueType IssueType => OperationOutcome.IssueType.Processing;
+}
