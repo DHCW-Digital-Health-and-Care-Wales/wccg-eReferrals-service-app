940582db69277eda37ebcb009750b3ede0d54786
Patryk Urban <1850832+urbanek32@users.noreply.github.com>
2026-03-23 11:51:48 +0100

    Add suppression attribute for static member declaration in IBundleModel interface

--- src/NWRI.eReferralsService.API/Models/IBundleModel.cs
+++ src/NWRI.eReferralsService.API/Models/IBundleModel.cs
@@ -1,8 +1,10 @@
+using System.Diagnostics.CodeAnalysis;
 using Hl7.Fhir.Model;
 
 namespace NWRI.eReferralsService.API.Models;
 
 public interface IBundleModel<T>
 {
+    [SuppressMessage("Design", "CA1000:Do not declare static members on generic types")]
     static abstract T FromBundle(Bundle bundle);
 }
