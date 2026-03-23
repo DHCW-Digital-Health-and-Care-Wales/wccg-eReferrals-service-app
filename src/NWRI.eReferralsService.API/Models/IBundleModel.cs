7004189c48d19bcbd1304070599689becf3b3913
Patryk Urban <1850832+urbanek32@users.noreply.github.com>
2026-03-23 12:16:43 +0100

    Refactor hello-world.yml trigger configuration and update suppression attribute in IBundleModel interface

--- src/NWRI.eReferralsService.API/Models/IBundleModel.cs
+++ src/NWRI.eReferralsService.API/Models/IBundleModel.cs
@@ -5,6 +5,7 @@ namespace NWRI.eReferralsService.API.Models;
 
 public interface IBundleModel<T>
 {
-    [SuppressMessage("Design", "CA1000:Do not declare static members on generic types")]
+    [SuppressMessage("Design", "CA1000:Do not declare static members on generic types",
+        Justification = "Static factory method for creating model instances from a FHIR Bundle.")]
     static abstract T FromBundle(Bundle bundle);
 }
